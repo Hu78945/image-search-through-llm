dc8af456370fe3aa92a14abb12aa749e859b7ae3
Software Engineer - FE <sohaibanwaar36@gmail.com>
2024-09-04 13:08:22 +0500

    module_6 done

--- src/components/Module6.jsx
+++ src/components/Module6.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Layout from './Layout'
 import Header from './shared/Header'
+import logo from '../click_logo_textless.svg'
 
 const Module6 = () => {
   return (
-    <div className='flex bg-black h-screen'>
-      <div className='w-3/4 border'>
-        <Header />
+    <div className='flex bg-black h-fit min-h-screen'>
+      <div className='w-3/4'>
+        <div className='fixed w-3/4'><Header /></div>
         <div className='flex justify-center items-center border border-red-900 h-full'>
           <div className='border w-1/2 relative'>
           <img src='./icons/click_logo_textless.png' className='absolute top-2 right-2 w-5 h-5'></img>
-            <img src='./icons/module_3a.png' className='w-full h-60' alt='Module'></img>
+            <img src='./icons/hero_image.png' className='w-full h-60' alt='Module'></img>
             <div className='flex justify-between p-2'>
               <div>
                 <p className='text-10 text-white uppercase'>Monday・September 26, 2023・14:45</p>
@@ -36,8 +37,8 @@ const Module6 = () => {
               <img src='./icons/signature.png'></img>
               <p className='text-10 text-white'>Monday・September 26, 2023・14:45:12 (cet)</p>
             </div>
-            <div className='bg-custom-gradient-2 h-12 w-20 rounded-2xl'>
-              <img src='./icons/click_logo.png'></img>
+            <div className='bg-custom-gradient-2 h-12 w-20 rounded-2xl flex justify-center items-center'>
+              <img src={logo}></img>
             </div>
           </div>
         </div>
@@ -52,7 +53,7 @@ const Module6 = () => {
             </div>
           </div>
 
-          <div className='border-b border-dark-gray py-6'>
+          <div className='border-b border-dark-gray py-10'>
             <p className='text-10 text-medium-gray mb-2'>DEVICE AND CAMERA</p>
 
             <p className='text-13 text-white uppercase'>iPhone 14 Pro</p>
@@ -65,11 +66,8 @@ const Module6 = () => {
             <p className='text-10 text-medium-gray mb-2'>VIDEO SETTINGS</p>
             <p className='text-13 text-white'>0:07 S・MOV・3024x4032・21.7 MB</p>
           </div>
-          <div className='flex justify-between gap-2 pt-8'>
-            <div className='w-4 h-4 bg-white rounded-full flex justify-center items-center'>
-              <img src='./icons/click_logo_textless.png' className=' top-2 right-2 w-3 h-3'></img>
-              
-            </div>
+          <div className='flex justify-between gap-2 pt-10'>
+            <img src='./icons/power_icon.png' className='h-4'></img>
             <p className='text-10 text-yellow'>POWERED BY NODLE CONTENTSIGN SDK</p>
           </div>
         </div>
